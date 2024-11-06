056e486aafd3d0a645af2cbc1ec5e9c5db08d5d0
Christelle CHARLES-ALFRED <christellecharlesalfred56@gmail.com>
2024-11-06 21:54:56 +0100

    initialisation du projet

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Annoucement from '../../components/announcement/Annoucement';
+
+function Home() {
+    return (
+        <div>
+            <Annoucement />
+        </div>
+    );
+}
+
+export default Home;
