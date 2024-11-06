532d5124e0099374473e04cb2ff6104dde409538
Christelle CHARLES-ALFRED <christellecharlesalfred56@gmail.com>
2024-11-06 23:55:11 +0100

    mise en place du slider et de la navbar

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Annoucement from '../../components/announcement/Annoucement';
+import Navbar from '../../components/navbar/Navbar';
+import Slider from '../../components/slider/Slider';
 
 function Home() {
     return (
         <div>
             <Annoucement />
+            <Navbar />
+            <Slider />
         </div>
     );
 }
