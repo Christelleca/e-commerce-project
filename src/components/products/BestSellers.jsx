c6037d52c6e67cbe64f26a3692968d5d67d7b2d1
Christelle CHARLES-ALFRED <christellecharlesalfred56@gmail.com>
2024-11-07 19:41:20 +0100

    initialisation des routes et de la page d'accueil

---
+++ src/components/products/BestSellers.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Products from './Products';
+import { bestsellers } from '../../data';
+
+function BestSellers() {
+    return <Products items={bestsellers} heading="Best Sellers" />;
+}
+
+export default BestSellers;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import Annoucement from '../../components/announcement/Annoucement';
 import Navbar from '../../components/navbar/Navbar';
 import Slider from '../../components/slider/Slider';
+import Categories from '../../components/categories/Categories';
+import NewArrivals from '../../components/products/NewArrivals';
+import BestSellers from '../../components/products/BestSellers';
+import Newsletter from '../../components/newsletter/Newsletter';
+import Footer from '../../components/footer/Footer';
 
 function Home() {
     return (
@@ -9,6 +14,11 @@ function Home() {
             <Annoucement />
             <Navbar />
             <Slider />
+            <Categories />
+            <NewArrivals />
+            <BestSellers />
+            <Newsletter />
+            <Footer />
         </div>
     );
 }
